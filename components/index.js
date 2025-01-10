45e90517589f25dc6838a4ba002aaaa2bd91f970
tydy234 <thiendark234@gmail.com>
2025-01-10 22:11:03 +0700

    first commit

---
+++ components/index.js
@@ -0,0 +1,12 @@
+import FormField from "./FormField";
+import Loader from "./Loader";
+import SearchInput from "./SearchInput";
+import EmptyState from "./EmptyState";
+import CustomButton from "./CustomButton";
+export {
+  FormField,
+  CustomButton,
+  Loader,
+  SearchInput,
+  EmptyState,
+};
